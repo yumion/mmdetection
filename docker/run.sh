511f3b5acc5142186bf06b47c18509b9736c3348
yumion <a.yamada0703@gmail.com>
2024-01-31 19:17:24 +0900

    fix: nvidia gpl key

--- docker/run.sh
+++ docker/run.sh
@@ -1,16 +1,27 @@
 CONTAINER_NAME=$1
+USER_ID=$(id -u)
+GROUP_ID=$(id -g)
+USER_NAME=$(whoami)
 
-cd /data2/src/atsushi/mmdetection
+
+cd /mnt/cloudy_z/src/atsushi/mmdetection
 docker rm -f $CONTAINER_NAME && \
 docker run \
     --gpus all \
     --shm-size=64gb \
     --restart unless-stopped\
-    -v /data1:/data1 \
-    -v /data2:/data2 \
+    -v /mnt:/mnt \
     -itd \
     --name $CONTAINER_NAME \
-    mmdetection:pytorch1.12.1-cuda11.3-cudnn8-mmcv2.0.1-mmengine0.8.4
+    mmdetection:pytorch1.12.1-cuda11.3-cudnn8-mmcv2.1.0-mmengine0.9.1
 
 docker exec -it $CONTAINER_NAME bash -c \
-    "cd $PWD && pip install --no-cache-dir -e ."
+    "groupadd -g $GROUP_ID $USER_NAME && useradd -d $PWD -u $USER_ID -g $GROUP_ID $USER_NAME"
+
+docker exec -it $CONTAINER_NAME bash -c \
+    "\
+    cd $PWD \
+    && pip install -r requirements/albu.txt \
+    && pip install -r requirements/optional.txt \
+    && pip install --no-cache-dir -e .\
+    "
